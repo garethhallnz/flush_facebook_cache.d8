86523241d01e4398cd54cfea141421fee9816598
Gareth Hall <gareth.hall@communica.co.nz>
2017-01-03 00:50:40 +1300

    Create Service

---
+++ src/FacebookFlushCacheService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\facebook_flush_cache;
+
+use GuzzleHttp\Exception\RequestException;
+
+/**
+ * Class FacebookService.
+ *
+ * @package Drupal\facebook_flush_cache
+ */
+class FacebookFlushCacheService {
+
+  /**
+   * The base url.
+   */
+  public $facebookUrl = 'https://graph.facebook.com';
+
+  /**
+   * Provides HTTP client service.
+   */
+  protected $httpClient;
+
+  /**
+   * Constructs a new object.
+   */
+  public function __construct() {
+    $this->httpClient = $client = \Drupal::httpClient();
+  }
+
+  /**
+   * Clear make request to clear cache.
+   */
+  public function execute($uri) {
+
+    try {
+
+      $url = $this->facebookUrl . '/?id=' . $uri . '&scrape=true';
+
+      $request = $this->httpClient->get($url);
+
+      $this->log($request->getBody());
+
+    }
+    catch (RequestException $error) {
+      $this->logError($error);
+    }
+
+  }
+
+  /**
+   * Log successful .
+   */
+  public function log($data) {
+  //    watchdog(
+//      'Facebook cache',
+//      'Flush FB Node: @nid <pre>@data<pre>',
+//      array('@nid' => $node->nid, '@data' => print_r($response, TRUE))
+//    );
+  }
+
+  /**
+   * Log Error.
+   */
+  public function logError($error) {
+    //    watchdog(
+//      'Facebook cache',
+//      'Flush FB Node: @nid <pre>@data<pre>',
+//      array('@nid' => $node->nid, '@data' => print_r($response, TRUE))
+//    );
+  }
+}
--- src/Plugin/Action/FacebookFlushCache.php
+++ src/Plugin/Action/FacebookFlushCache.php
@@ -4,6 +4,8 @@ namespace Drupal\facebook_flush_cache\Plugin\Action;
 
 use Drupal\Core\Action\ActionBase;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Drupal\facebook_flush_cache\FacebookFlushCacheService;
 
 /**
  * Flush Facebook cache for given node and its url.
@@ -21,35 +23,11 @@ class FacebookFlushCache extends ActionBase {
    */
   public function execute($entity = NULL) {
 
-    $url = \Drupal\Core\Url::fromRoute('entity.node.canonical', ['node' => $entity->id()], ['absolute' => TRUE]);
+    $url = Url::fromRoute('entity.node.canonical', ['node' => $entity->id()], ['absolute' => TRUE]);
 
-    $client = \Drupal::httpClient();
-    $request = $client->post('https://graph.facebook.com/?id=' . $url->toString() . '&scrape=true');
+    $service = new FacebookFlushCacheService();
 
-    kint($request->getBody()->getContents());
-    die;
-
-
-
-
-
-//    $url = drupal_get_path_alias('node/' . $node->nid);
-//
-//    $page_url = $base_url . '/' . $url;
-//
-//    $fb_url = 'https://graph.facebook.com/?id=' . $page_url . '&scrape=true';
-//
-//    $response = drupal_http_request($fb_url, array('method' => 'POST'));
-//
-//    watchdog(
-//      'Facebook cache',
-//      'Flush FB Node: @nid <pre>@data<pre>',
-//      array('@nid' => $node->nid, '@data' => print_r($response, TRUE))
-//    );
-//
-//    return $response;
-
-
+    $service->execute($url->toString());
 
   }
 
