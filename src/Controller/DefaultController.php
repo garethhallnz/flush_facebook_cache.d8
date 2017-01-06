daa9cbbac0b2872c5ba6ef2579283fe59dcc9dea
Gareth Hall <gareth.hall@communica.co.nz>
2017-01-06 23:09:54 +1300

    git commit -m 'Issue #2826284 by garethhallnz: Port to D8'

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,9 @@
 namespace Drupal\facebook_flush_cache\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 use Drupal\facebook_flush_cache\FacebookFlushCacheService;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Default controller for the simple_currency_converter module.
@@ -13,14 +15,22 @@ class DefaultController extends ControllerBase {
   /**
    * {@inheritdoc}
    */
-  public function execute($entity = NULL) {
+  public function execute($nid) {
 
-    $url = Url::fromRoute('entity.node.canonical', ['node' => $entity->id()], ['absolute' => TRUE]);
+    $url = Url::fromRoute('entity.node.canonical', ['node' => $nid], ['absolute' => TRUE]);
 
-    $service = new FacebookFlushCacheService();
+    if ($url) {
 
-    $service->execute($url->toString());
+      $service = new FacebookFlushCacheService();
 
+      $url = $url->toString();
+
+      $service->execute($url);
+
+      return new RedirectResponse($url);
+    }
+
+    // @todo Need to return a Response
   }
 
 }
